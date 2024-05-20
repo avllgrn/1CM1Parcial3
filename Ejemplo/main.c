eb2c713bf46a0175eb4ac5955214080076b25879
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-05-19 18:52:43 -0600

    Ciclo infinito, mostrando un mensaje

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -3,10 +3,9 @@
 
 int main(void){
 
-    do{//Mientras la condicion sea verdadera (1)
-        //Repite este conjunto de instrucciones
-        printf("Hola! =^) ");
-    }while( 0 );
-    //El conjunto de instrucciones SE ejecuta al menos UNA vez
+    while(1){
+        printf("1 ");
+    }
+
     return 0;
 }
