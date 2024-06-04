4216c7b34bee021a6fcaf0f8e12f2361562880be
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-03 18:22:05 -0600

    Sumas acumuladas

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -2,51 +2,219 @@
 #include <stdlib.h>
 #include <math.h>
 
-void contadorAscendente(int ini, int fin, int paso);
-void contadorDescendente(int ini, int fin, int paso);
+int esPar(int x);
+int factorial(int x);
+float gradosARadianes(float grados);
+void sumaAcumulada1(void);
+void sumaAcumulada2(void);
+void sumaAcumulada3(void);
+void sumaAcumulada4(void);
+void sumaAcumulada5(void);
+void sumaAcumulada6(void);
+void sumaAcumulada7(void);
+void sumaAcumulada8(void);
+void sumaAcumulada9(void);
 
 int main(void){
 
-    int ini,fin,paso;
-
-    printf("Ingresa ini ");scanf("%d",&ini);
-    printf("Ingresa fin ");scanf("%d",&fin);
-    printf("Ingresa paso ");scanf("%d",&paso);
-    paso = abs(paso);
-
-    if(ini <= fin){
-        contadorAscendente(ini,fin,paso);
-    }
-    else{
-        contadorDescendente(ini,fin,paso);
-    }
+    sumaAcumulada1();
+    sumaAcumulada2();
+    sumaAcumulada3();
+    sumaAcumulada4();
+    sumaAcumulada5();
+    sumaAcumulada6();
+    sumaAcumulada7();
+    sumaAcumulada8();
+    sumaAcumulada9();
 
     return 0;
 }
 
-void contadorAscendente(int ini, int fin, int paso){
-    int i;
-    paso = abs(paso);
-    if(ini <= fin && paso>0){
-
-        i=ini;
-        while(i <= fin){
-            printf("%d ",i);
-            i = i+paso;
-        }
-
-    }
+int esPar(int x){
+    return x%2 == 0;
 }
-void contadorDescendente(int ini, int fin, int paso){
-    int i;
-    paso = abs(paso);
-    if(ini >= fin && paso>0){
+int factorial(int x){
+    int i,f;
+    for(i=1,f=1; i<=x; i++)
+        f *= i;
+    return f;
+}
+float gradosARadianes(float grados){
+    return grados*M_PI/180;
+}
+void sumaAcumulada1(void){
+    int i,n;
+    float s;
+    system("cls");
+    printf("Calculo de 0+1+2+3+4+...+n\n\n");
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    printf("\n\n");
 
-        i=ini;
-        while(i >= fin){
-            printf("%d ",i);
-            i = i-paso;
-        }
-
+    for(i=0, s=0; i<=n; i++){
+        printf("i=%d\t| s=%f\n",i,s);
+        s += i;
     }
+    printf("\n\ns = %f\n\n",s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada2(void){
+    int i,n;
+    float s;
+    system("cls");
+    printf("Calculo de 0+2+4+6+8+...+n\n\n");
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    printf("\n\n");
+
+    for(i=0, s=0; i<=n; i+=2){
+        printf("i=%d\t| s=%f\n",i,s);
+        s += i;
+    }
+    printf("\n\ns = %f\n\n",s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada3(void){
+    int i,n;
+    float s;
+    system("cls");
+    printf("Calculo de 0-1+2-3+4-...+n\n\n");
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    printf("\n\n");
+
+    for(i=0, s=0; i<=n; i++){
+        printf("i=%d\t| s=%f\n",i,s);
+        if(esPar(i))
+            s += i;
+        else
+            s -= i;
+    }
+    printf("\n\ns = %f\n\n",s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada4(void){
+    int i,n;
+    float s;
+    system("cls");
+    printf("Calculo de 0+1-2+3-4+...-n\n\n");
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    printf("\n\n");
+    for(i=0, s=0; i<=n; i++){
+        printf("i=%d\t| s=%f\n",i,s);
+        if(!esPar(i))
+            s += i;
+        else
+            s -= i;
+    }
+    printf("\n\ns = %f\n\n",s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada5(void){
+    int i,n;
+    float s,x;
+    system("cls");
+    printf("Calculo de x^0 + x^1 + x^2 +...+ x^n\n\n");
+    printf("Ingresa x ");
+    scanf("%f",&x);
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    printf("\n\n");
+
+    for(i=0, s=0; i<n; i++){
+        printf("i=%d\t| s=%f\n",i,s);
+        s += pow(x,i);
+    }
+    printf("\n\ns = %f\n\n",s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada6(void){
+    int i,n;
+    float s,x;
+    system("cls");
+    printf("Calculo de (x^1)/1 + (x^2)/2 + (x^3)/3 +...+ (x^n)/n\n\n");
+    printf("Ingresa x ");
+    scanf("%f",&x);
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    printf("\n\n");
+
+    for(i=1, s=0; i<n; i++){
+        printf("i=%d\t| s=%f\n",i,s);
+        s += pow(x,i)/i;
+    }
+    printf("\n\ns = %f\n\n",s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada7(void){
+    int i,n;
+    float s,x;
+    system("cls");
+    printf("Calculo de e^x\n\n");
+    printf("Ingresa x ");
+    scanf("%f",&x);
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    printf("\n\n");
+
+    for(i=0, s=0; i<n; i++){
+        printf("i=%d\t| s=%f\n",i,s);
+        s += pow(x,i)/factorial(i);
+    }
+    printf("\n\ne^%f = %f\n\n",x,s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada8(void){
+    int i,j,n;
+    float s,x,xRad;
+    system("cls");
+    printf("Calculo de cos(x)\n\n");
+    printf("Ingresa x ");
+    scanf("%f",&x);
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    xRad = gradosARadianes(x);
+    printf("\n\n");
+
+    for(i=0, j=0, s=0; i<=n; i++, j+=2){
+        printf("i=%d\tj=%d\t| s=%f\n",i,j,s);
+        if(esPar(i))
+            s += pow(xRad,j)/factorial(j);
+        else
+            s -= pow(xRad,j)/factorial(j);
+    }
+    printf("\n\ncos(%f) = %f\n\n",x,s);
+    system("pause");
+    system("cls");
+}
+void sumaAcumulada9(void){
+    int i,j,n;
+    float s,x,xRad;
+    system("cls");
+    printf("Calculo de sen(x)\n\n");
+    printf("Ingresa x ");
+    scanf("%f",&x);
+    printf("Ingresa n ");
+    scanf("%d",&n);
+    xRad = gradosARadianes(x);
+    printf("\n\n");
+
+    for(i=0, j=1, s=0; i<=n; i++, j+=2){
+        printf("i=%d\tj=%d\t| s=%f\n",i,j,s);
+        if(esPar(i))
+            s += pow(xRad,j)/factorial(j);
+        else
+            s -= pow(xRad,j)/factorial(j);
+    }
+    printf("\n\nsen(%f) = %f\n\n",x,s);
+    system("pause");
+    system("cls");
 }
