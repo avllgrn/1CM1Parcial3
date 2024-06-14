8f2cd1fb0cd1ab4de47be216c50040180dbbf0d1
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-14 11:18:00 -0600

    Mostrar una matriz (con basura), de izqquierda a derecha, arriba a abajo

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -2,17 +2,18 @@
 
 int main(void){
     float M[50][50];
+    int i,j,m,n;
 
-    M[1][2] = 1.2;
+    printf("Ingresa m ");scanf("%d",&m);
+    printf("Ingresa n ");scanf("%d",&n);
 
-    printf("Ingresa un valor para M[3][4] ");
-    scanf("%f",&M[3][4]);
-
-    M[5][6] = M[1][2] * M[3][4];
-
-    printf("\nM[1][2] = %f\n",M[1][2]);
-    printf("\nM[3][4] = %f\n",M[3][4]);
-    printf("\nM[5][6] = M[1][2] * M[3][4] = %f\n",M[5][6]);
+    printf("M\n");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            printf("%f\t",M[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
