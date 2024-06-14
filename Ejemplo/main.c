8ecc4968bf77d8768b4e707dca334f8124fbcca6
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-14 11:19:52 -0600

    Leer y mostrar una matriz de izqquierda a derecha, arriba a abajo

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -7,7 +7,23 @@ int main(void){
     printf("Ingresa m ");scanf("%d",&m);
     printf("Ingresa n ");scanf("%d",&n);
 
-    printf("M\n");
+    printf("\nM (con basura)\n");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            printf("%f\t",M[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nM\n");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            printf("Ingresa [%d][%d] ",i,j);
+            scanf("%f",&M[i][j]);
+        }
+    }
+
+    printf("\nM\n");
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
             printf("%f\t",M[i][j]);
