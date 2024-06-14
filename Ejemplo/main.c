e778062c9f6b7bdaca67120f373845cbd6847041
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-14 11:21:28 -0600

    Rellenar con valores aleatorios una matriz de izqquierda a derecha, arriba a abajo

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void){
-    float M[50][50];
+    srand(time(NULL));
+    int M[50][50];
     int i,j,m,n;
 
     printf("Ingresa m ");scanf("%d",&m);
     printf("Ingresa n ");scanf("%d",&n);
 
-    printf("\nM (con basura)\n");
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
-            printf("%f\t",M[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\nM\n");
-    for(i=0; i<m; i++){
-        for(j=0; j<n; j++){
-            printf("Ingresa [%d][%d] ",i,j);
-            scanf("%f",&M[i][j]);
+            M[i][j] = rand()%100;
         }
     }
 
     printf("\nM\n");
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
-            printf("%f\t",M[i][j]);
+            printf("%d\t",M[i][j]);
         }
         printf("\n");
     }
