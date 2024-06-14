3a61185cfff8ce03e2c8b970999d8d05f68027b7
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-14 11:25:05 -0600

    Genera Matriz Transpuesta

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -4,24 +4,37 @@
 
 int main(void){
     srand(time(NULL));
-    int M[50][50];
-    int i,j,n;
+    int M[50][50],MT[50][50];
+    int i,j,m,n,mT,nT;
 
+    printf("Ingresa m ");scanf("%d",&m);
     printf("Ingresa n ");scanf("%d",&n);
+    mT = n;
+    nT = m;
 
-    for(i=0; i<n; i++){
+    for(i=0; i<m; i++){
         for(j=0; j<n; j++){
-            if(i==j)
-                M[i][j] = 1;
-            else
-                M[i][j] = 0;
+            M[i][j] = rand()%100;
+        }
+    }
+
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            MT[j][i] = M[i][j];
         }
     }
 
     printf("\nM\n");
-    for(i=0; i<n; i++){
+    for(i=0; i<m; i++){
         for(j=0; j<n; j++){
             printf("%d\t",M[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nMT\n");
+    for(i=0; i<mT; i++){
+        for(j=0; j<nT; j++){
+            printf("%d\t",MT[i][j]);
         }
         printf("\n");
     }
