c1ecaf2e0f191afbb234b2e489857550e8a6fb25
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-14 11:15:21 -0600

    Ejemplos iniciales de Matriz

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-    int i,j,m,n;
+    float M[50][50];
 
-    printf("Ingresa m ");scanf("%d",&m);
-    printf("Ingresa n ");scanf("%d",&n);
+    M[1][2] = 1.2;
 
-    for(i=0; i<m; i++){
-        for(j=0; j<n; j++){
-            printf("[%d][%d]\t",i,j);
-        }
-        printf("\n");
-    }
+    printf("Ingresa un valor para M[3][4] ");
+    scanf("%f",&M[3][4]);
+
+    M[5][6] = M[1][2] * M[3][4];
+
+    printf("\nM[1][2] = %f\n",M[1][2]);
+    printf("\nM[3][4] = %f\n",M[3][4]);
+    printf("\nM[5][6] = M[1][2] * M[3][4] = %f\n",M[5][6]);
 
     return 0;
 }
