4961729fd5c3d7e314f7e6522d15f6ebc7e5453b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-18 14:26:44 -0600

    Unos en diagonal principal o en diagonal secundaria

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,48 +1,32 @@
 #include <stdio.h>
 
-void muestraIzquierdaDerechaArribaAbajo(int m, int n, int X[50][50]);
-void rellenaIzquierdaDerechaArribaAbajo(int m, int n, int X[50][50]);
-void rellenaArribaAbajoIzquierdaDerecha(int m, int n, int X[50][50]);
-void rellenaDerechaIzquierdaArribaAbajo(int m, int n, int X[50][50]);
-void rellenaArribaAbajoDerechaIzquierda(int m, int n, int X[50][50]);
-void rellenaIzquierdaDerechaAbajoArriba(int m, int n, int X[50][50]);
-void rellenaAbajoArribaIzquierdaDerecha(int m, int n, int X[50][50]);
-void rellenaDerechaIzquierdaAbajoArriba(int m, int n, int X[50][50]);
-void rellenaAbajoArribaDerechaIzquierda(int m, int n, int X[50][50]);
+void muestraMatriz(int m, int n, int X[50][50]);
+void inicializaMatriz(int m, int n, int X[50][50]);
+void diagonalPrincipalUnos(int n, int X[50][50]);
+void diagonalSecundariaUnos(int n, int X[50][50]);
 
 int main(void){
-    int M[50][50],m,n;
-    printf("Ingresa m ");scanf("%d",&m);
+    int M[50][50],n;
+
     printf("Ingresa n ");scanf("%d",&n);
 
-    rellenaIzquierdaDerechaArribaAbajo(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
+    inicializaMatriz(n,n,M);
+    muestraMatriz(n,n,M);
 
-    rellenaArribaAbajoIzquierdaDerecha(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
+    diagonalPrincipalUnos(n,M);
+    muestraMatriz(n,n,M);
 
-    rellenaDerechaIzquierdaArribaAbajo(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
+    inicializaMatriz(n,n,M);
+    muestraMatriz(n,n,M);
 
-    rellenaArribaAbajoDerechaIzquierda(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
-
-    rellenaIzquierdaDerechaAbajoArriba(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
-
-    rellenaAbajoArribaIzquierdaDerecha(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
-
-    rellenaDerechaIzquierdaAbajoArriba(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
-
-    rellenaAbajoArribaDerechaIzquierda(m,n,M);
-    muestraIzquierdaDerechaArribaAbajo(m,n,M);
+    diagonalSecundariaUnos(n,M);
+    muestraMatriz(n,n,M);
 
     return 0;
 }
 
-void muestraIzquierdaDerechaArribaAbajo(int m, int n, int X[50][50]){//Siempre se muestra, De izquierda a Derecha, De arriba hacia abajo
+//Siempre se muestra, De izquierda a Derecha, De arriba hacia abajo
+void muestraMatriz(int m, int n, int X[50][50]){
     int i,j;
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
@@ -52,52 +36,23 @@ void muestraIzquierdaDerechaArribaAbajo(int m, int n, int X[50][50]){//Siempre s
     }
     printf("\n\n");
 }
-void rellenaIzquierdaDerechaArribaAbajo(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(i=0; i<m; i++)
+void inicializaMatriz(int m, int n, int X[50][50]){
+    int i,j;
+    for(i=0; i<n; i++)
         for(j=0; j<n; j++)
-            X[i][j] = cont++;
-
+            X[i][j] = 0;
 }
-void rellenaArribaAbajoIzquierdaDerecha(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(j=0; j<n; j++)
-        for(i=0; i<m; i++)
-            X[i][j] = cont++;
-}
-void rellenaDerechaIzquierdaArribaAbajo(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(i=0; i<m; i++)
-        for(j=n-1; j>=0; j--)
-            X[i][j] = cont++;
-}
-void rellenaArribaAbajoDerechaIzquierda(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(j=n-1; j>=0; j--)
-        for(i=0; i<m; i++)
-            X[i][j] = cont++;
-}
-void rellenaIzquierdaDerechaAbajoArriba(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(i=m-1; i>=0; i--)
+void diagonalPrincipalUnos(int n, int X[50][50]){
+    int i,j;
+    for(i=0; i<n; i++)
         for(j=0; j<n; j++)
-            X[i][j] = cont++;
+            if(i == j)
+                X[i][j] = 1;
 }
-void rellenaAbajoArribaIzquierdaDerecha(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(j=0; j<n; j++)
-        for(i=m-1; i>=0; i--)
-            X[i][j] = cont++;
-}
-void rellenaDerechaIzquierdaAbajoArriba(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(i=m-1; i>=0; i--)
-        for(j=n-1; j>=0; j--)
-            X[i][j] = cont++;
-}
-void rellenaAbajoArribaDerechaIzquierda(int m, int n, int X[50][50]){
-    int i,j,cont=1;
-    for(j=n-1; j>=0; j--)
-        for(i=m-1; i>=0; i--)
-            X[i][j] = cont++;
+void diagonalSecundariaUnos(int n, int X[50][50]){
+    int i,j;
+    for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+            if(i == n-j-1)
+                X[i][j] = 1;
 }
